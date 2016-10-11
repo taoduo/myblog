2db5bf856aaa85c13ebe9d2442e5a1e777e5a5e1
Duo Tao <taod@carleton.edu>
2016-10-10 22:23:21 -0500

    setup multer for uploading pics

--- routes/management.js
+++ routes/management.js
@@ -5,12 +5,24 @@ var User = require(__public + 'models/user.js');
 var Post = require(__public + 'models/post.js');
 var blog = require(__public + 'api/api.blog.js');
 var Location = require(__public + 'api/api.location.js');
+var multer = require('multer');
+var storage = multer.diskStorage({
+   destination: function (req, file, cb) {
+      cb(null, '../public/images/blog-img/')
+   },
+   filename: function (req, file, cb) {
+      cb(null, file.originalname);
+   }
+});
+var uploading = multer({
+   storage: storage,
+});
 
 var isAuthenticated = function (req, res, next) {
   if (req.isAuthenticated())
     return next();
   res.redirect('/');
-}
+};
 
 module.exports = function(passport) {
   /* GET administrator page. */
